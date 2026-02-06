5497f3e32b6da7c2ef251dd170001cb2c7d4ea62
JonasHri <jonasharriehausen@gmail.com>
2026-02-06 12:25:45 +0100

    tree sampling now handled by local model

--- src/frsf/models.py
+++ src/frsf/models.py
@@ -7,6 +7,20 @@ import pandas as pd
 
 
 class LocalRandomSurvivalForest(RandomSurvivalForest):
+    def __init__(
+        self,
+        update_method: Literal["constant", "all"] = "all",
+        update_weighting: Literal["equal", "site_size"] = "equal",
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.local_features: set = set()
+        self.federated_estimators: list[SurvivalTree] = []
+        self.site_size: int = 0
+        self.update_method = update_method
+        self.update_weighting = update_weighting
+        self.tree_origin: Literal["local", "federated"] = "local"
+
     def fit(
         self,
         X: pd.DataFrame,
@@ -15,10 +29,53 @@ class LocalRandomSurvivalForest(RandomSurvivalForest):
     ) -> Self:
         self.site_size: int = len(X)
         self.local_features = set(X.columns[~X.isna().all()].tolist())
+        self.all_features = X.columns.tolist()
         super().fit(X, y, sample_weight=sample_weight)
         for estimator in self.estimators_:
             estimator.sample_weight = self.site_size / self.n_estimators
 
+        return self
+
+    def set_federated_estimators(self, estimators: list[SurvivalTree]):
+        self.federated_estimators = estimators
+        return self
+
+    def use_local_estimators(self):
+        if self.tree_origin == "local":
+            return self
+
+        self.tree_origin = "local"
+
+        self.estimators_ = self.local_estimators
+        self.n_estimators = len(self.estimators_)
+
+    def use_federated_estimators(self, random_state: int = None):
+        if random_state is None:
+            random_state = self.random_state
+
+        if self.tree_origin == "federated":
+            return self
+
+        self.local_estimators = self.estimators_
+        self.tree_origin = "federated"
+
+        if self.update_method == "all":
+            self.estimators_ = self.federated_estimators
+
+        elif self.update_method == "constant":
+            if self.update_weighting == "equal":
+                weights: list[float] = [1.0] * len(self.federated_estimators)
+            elif self.update_weighting == "site_size":
+                weights: list[float] = [
+                    estimator.sample_weight for estimator in self.federated_estimators
+                ]
+            self.estimators_ = np.random.default_rng(random_state).choice(
+                self.federated_estimators,
+                size=self.n_estimators,
+                p=np.array(weights) / sum(weights),
+            )
+
+        self.n_estimators = len(self.estimators_)
         return self
 
     def save(self, path):
@@ -33,14 +90,10 @@ class FederatedRandomSurvivalForest(RandomSurvivalForest):
     def __init__(
         self,
         local_models: list[LocalRandomSurvivalForest],
-        local_update_method: Literal["constant", "all"] = "all",
-        local_update_weighting: Literal["equal", "site_size"] = "equal",
         **kwargs,
     ):
         super().__init__(**kwargs)
-        self.local_update_method = local_update_method
-        self.local_update_weighting = local_update_weighting
-        self.feature_names_in_: np.ndarray = local_models[0].feature_names_in_
+        self.all_features: np.ndarray = local_models[0].all_features
 
         self.local_models: list[LocalRandomSurvivalForest] = []
         self.estimators_: list[SurvivalTree] = []
@@ -53,7 +106,7 @@ class FederatedRandomSurvivalForest(RandomSurvivalForest):
         for estimator in local_model.estimators_:
             tree_features = estimator.tree_.feature
             tree_features_names = set(
-                [self.feature_names_in_[i] for i in tree_features if i >= 0]
+                [self.all_features[i] for i in tree_features if i >= 0]
             )
             local_tree_features.append(tree_features_names)
 
@@ -66,7 +119,10 @@ class FederatedRandomSurvivalForest(RandomSurvivalForest):
     def fit(self, *args, **kwargs):
         raise NotImplementedError("Federated model cannot be fit directly.")
 
-    def update_local_models(self, random_state: int = None):
+    def predict(self, X):
+        raise NotImplementedError("Federated model cannot predict directly.")
+
+    def distribute_trees(self, random_state: int = None):
         for model in self.local_models:
             valid_estimators = []
             for estimator, feat_set in zip(self.estimators_, self.tree_features):
@@ -74,26 +130,11 @@ class FederatedRandomSurvivalForest(RandomSurvivalForest):
                 if feat_set.issubset(model.local_features):
                     valid_estimators.append(estimator)
 
-            if self.local_update_method == "all":
-                model.estimators_ = valid_estimators
-                model.n_estimators = len(model.estimators_)
-
-            elif self.local_update_method == "constant":
-                if self.local_update_weighting == "equal":
-                    weights: list[float] = [1.0] * len(model.estimators_)
-                elif self.local_update_weighting == "site_size":
-                    weights: list[float] = [
-                        estimator.sample_weight for estimator in valid_estimators
-                    ]
-                model.estimators_ = np.random.default_rng(random_state).choice(
-                    valid_estimators,
-                    size=model.n_estimators,
-                    p=np.array(weights) / sum(weights),
-                )
+            model.set_federated_estimators(valid_estimators)
 
     def save(self, path):
         joblib.dump(self, path)
 
     @classmethod
-    def from_file(cls, path) -> Self:
+    def load(cls, path) -> Self:
         return joblib.load(path)
